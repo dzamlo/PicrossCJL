125bae9c4b6e0a34c6cc3499db99fe95c993bfa7
Loïc Damien <dzamlo@example.com>
2014-02-10 11:38:36 +0100

    start of solver

--- PicrossCJL/PicrossCJL/PicrossSolver.cs
+++ PicrossCJL/PicrossCJL/PicrossSolver.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Drawing;
 
 namespace PicrossCJL
 {
@@ -12,9 +13,42 @@ namespace PicrossCJL
 
         }
 
-        void Solve(PicrossPuzzle puzzle)
+        bool Solve(PicrossPuzzle puzzle)
         {
+            //algo:
+            //  backtracking, remplir ce dont est sur en fonction de l'état de la grille entre chauqe appel recursif
 
+            //base case en fonction de puzzle.getgamestate
+
+
+            Size s = puzzle.Size;
+            int w = s.Width;
+            int h = s.Height;
+            // remplir ce dont on n'est sur
+
+            // pour chaque cellule avec des doutes:
+            // essayer remplir la cellule, essayer de résoudre (appel recursif à solve)
+            // si resolutuion ok -> fini
+            // sinon  -> cellule crossed, essayer de remplir la prochiane, etc.
+            for (int x = 0; x < w; x++)
+            {
+                for (int y = 0; y < h; y++)
+                {
+                    if (puzzle.Cells[x, y] == PicrossPuzzle.CellValue.Empty)
+                    {
+                        puzzle.Cells[x, y] = PicrossPuzzle.CellValue.Filled;
+                        PicrossPuzzle tmpPuzzle = new PicrossPuzzle(puzzle);
+                        if (Solve(tmpPuzzle))
+                        {
+                            puzzle.Cells = tmpPuzzle.Cells;
+                            return true;
+                        }
+                        puzzle.Cells[x, y] = PicrossPuzzle.CellValue.Crossed;
+                    }
+                }
+            }
+
+            return false;
         }
     }
 }
