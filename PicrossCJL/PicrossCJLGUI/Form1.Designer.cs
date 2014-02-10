d646b531ccea5b27492bf001579783aa16142541
peiryj <jordan.peiry@gmail.com>
2014-02-10 11:33:01 +0100

    Added some basic functions

--- PicrossCJL/PicrossCJLGUI/Form1.Designer.cs
+++ PicrossCJL/PicrossCJLGUI/Form1.Designer.cs
@@ -28,12 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fichierToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sauvegardezToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.chargerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.quitterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aProposToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.sfd = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fichierToolStripMenuItem,
+            this.aProposToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(584, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fichierToolStripMenuItem
+            // 
+            this.fichierToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sauvegardezToolStripMenuItem,
+            this.chargerToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.quitterToolStripMenuItem});
+            this.fichierToolStripMenuItem.Name = "fichierToolStripMenuItem";
+            this.fichierToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.fichierToolStripMenuItem.Text = "Fichier";
+            // 
+            // sauvegardezToolStripMenuItem
+            // 
+            this.sauvegardezToolStripMenuItem.Name = "sauvegardezToolStripMenuItem";
+            this.sauvegardezToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.sauvegardezToolStripMenuItem.Text = "Sauvegarder";
+            this.sauvegardezToolStripMenuItem.Click += new System.EventHandler(this.sauvegardezToolStripMenuItem_Click);
+            // 
+            // chargerToolStripMenuItem
+            // 
+            this.chargerToolStripMenuItem.Name = "chargerToolStripMenuItem";
+            this.chargerToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.chargerToolStripMenuItem.Text = "Charger";
+            this.chargerToolStripMenuItem.Click += new System.EventHandler(this.chargerToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(136, 6);
+            // 
+            // quitterToolStripMenuItem
+            // 
+            this.quitterToolStripMenuItem.Name = "quitterToolStripMenuItem";
+            this.quitterToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.quitterToolStripMenuItem.Text = "Quitter";
+            this.quitterToolStripMenuItem.Click += new System.EventHandler(this.quitterToolStripMenuItem_Click);
+            // 
+            // aProposToolStripMenuItem
+            // 
+            this.aProposToolStripMenuItem.Name = "aProposToolStripMenuItem";
+            this.aProposToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.aProposToolStripMenuItem.Text = "A propos";
+            this.aProposToolStripMenuItem.Click += new System.EventHandler(this.aProposToolStripMenuItem_Click);
+            // 
+            // ofd
+            // 
+            this.ofd.Filter = "Fichiers XML|*.xml";
+            // 
+            // sfd
+            // 
+            this.sfd.FileName = "MyPicrossGame";
+            this.sfd.Filter = "Fichiers XML|*.xml";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(584, 562);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Picross CJL";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fichierToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sauvegardezToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem chargerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem quitterToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog ofd;
+        private System.Windows.Forms.SaveFileDialog sfd;
+        private System.Windows.Forms.ToolStripMenuItem aProposToolStripMenuItem;
+
+
     }
 }
 
---
+++ PicrossCJL/PicrossCJLGUI/PicrossCJLView.cs
@@ -0,0 +1,63 @@
+﻿/*******************
+ * Author : J. Peiry
+ * Date   : 10.02.2014
+ * Desc   : GUI for the PicrossPuzzle application
+ * *****************/
+
+using System;
+using System.Windows.Forms;
+
+namespace PicrossCJLGUI
+{
+    public partial class Form1 : Form
+    {   
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// On click on the "sauvegarder" button in the menu. Save the file.
+        /// </summary>
+        private void sauvegardezToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                //save file
+            }
+        }
+
+        /// <summary>
+        /// On click on the "Quitter" button in the menu. Exit the application
+        /// </summary>
+        private void quitterToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Save data ? Confirmation before exit window ?
+            Application.Exit();
+        }
+
+        /// <summary>
+        /// On click on the "charger" button in the menu. Open an xml file
+        /// </summary>
+        private void chargerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                //load file
+            }
+        }
+
+        /// <summary>
+        /// On click on the "A propos" button in the menu
+        /// </summary>
+        private void aProposToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Created by Kris, Loic and Jordan");
+        }
+
+        
+
+    }
+}
