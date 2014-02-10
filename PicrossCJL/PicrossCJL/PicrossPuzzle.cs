3925f83b363aab6cce49313e5a8bf0550c2dc707
Loïc Damien <dzamlo@example.com>
2014-02-10 09:28:12 +0100

    Initial project structure

---
+++ PicrossCJL/PicrossCJL/PicrossPuzzle.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace PicrossCJL
+{
+    public class PicrossPuzzle
+    {
+        enum CellValue
+        {
+            Empty,
+            Filled,
+            Crossed
+        }
+
+        enum PuzzleState
+        {
+            Incorrect,
+            Finished,
+            Incomplete
+        }
+
+        public CellValue[,] Cells { get; set; }
+        public int[][] LinesValues { get; private set; }
+        public int[][] ColumnsValues { get; private set; }
+
+        public Size Size
+        {
+            get
+            {
+                return new Size();
+            }
+        }
+
+        public PicrossPuzzle(CellValue[,] cells, int[][] linesValues, int[][] columnsValues)
+        {
+
+        }
+
+        public PicrossPuzzle(string filename)
+        {
+
+        }
+
+        public PicrossPuzzle(PicrossPuzzle puzzle)
+        {
+
+        }
+
+        public PicrossPuzzle(Bitmap img, Size? size = null)
+        {
+
+        }
+
+        public void LoadFromFile(string filename)
+        {
+        }
+
+        public void SaveToFile(string filename)
+        {
+        }
+
+        public PuzzleState GetPuzzleState()
+        {
+            return PuzzleState.Incomplete;
+        }
+    }
+}
---
+++ PicrossCJL/PicrossCJL/PicrossSolver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PicrossCJL
+{
+    public class PicrossSolver
+    {
+        public PicrossSolver()
+        {
+
+        }
+
+        void Solve(PicrossPuzzle puzzle)
+        {
+
+        }
+    }
+}
