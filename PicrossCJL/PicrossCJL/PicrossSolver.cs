f15df0e82d091f69cb9ad98396b58af929647e85
Loïc Damien <dzamlo@example.com>
2014-03-10 08:12:39 +0100

    The solver is "working" but very slow

--- PicrossCJL/PicrossCJL/PicrossSolver.cs
+++ PicrossCJL/PicrossCJL/PicrossSolver.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Drawing;
+using System.Threading.Tasks;
 
 namespace PicrossCJL
 {
@@ -13,22 +14,61 @@ namespace PicrossCJL
 
         }
 
-        public bool Solve(PicrossPuzzle puzzle, int currentX = 0, int currentY  = 0)
+        /// <summary>
+        /// Fill all it can fill deterministicaly
+        /// </summary>
+        /// <param name="puzzle"></param>
+        /// <param name="lineNo"></param>
+        /// <returns>True if it make any progress</returns>
+        public bool SolveLine(PicrossPuzzle puzzle, int lineNo)
+        {
+            //Console.WriteLine(lineNo);
+            return false;
+        }
+
+        public bool SolveColumn(PicrossPuzzle puzzle, int columnNo)
+        {
+            return false;
+        }
+
+        public bool Any(IEnumerable<bool> bools)
+        {
+            foreach (var b in bools)
+            {
+                if(b) return true;
+            }
+            return false;
+        }
+
+        public bool Solve(PicrossPuzzle puzzle, int currentX = 0, int currentY = 0)
         {
             //algo:
-            //  backtracking, remplir ce dont est sur en fonction de l'état de la grille entre chauqe appel recursif
+            //  backtracking, remplir ce dont est sur en fonction de l'état de la grille entre chaque appel recursif
 
             //base case en fonction de puzzle.getgamestate
+
             PicrossPuzzle.PuzzleState puzzleState = puzzle.GetPuzzleState();
             if (puzzleState == PicrossPuzzle.PuzzleState.Finished)
                 return true;
             else if (puzzleState == PicrossPuzzle.PuzzleState.Incorrect)
-                return true;
+                return false;
 
             Size s = puzzle.Size;
             int w = s.Width;
             int h = s.Height;
+
             // remplir ce dont on n'est sur
+
+            bool[] progressLines = new bool[h];
+            bool[] progressColumn = new bool[w];
+
+
+            do
+            {
+              Parallel.For(currentX, h, i => { progressLines[i] = SolveLine(puzzle, i); });
+              Parallel.For(0, w, i => { progressColumn[i] = SolveColumn(puzzle, i); });
+            } while (Any(progressLines) || Any(progressColumn));
+
 
             // pour chaque cellule avec des doutes:
             // essayer remplir la cellule, essayer de résoudre (appel recursif à solve)
--- PicrossCJL/PicrossSolverCLI/Program.cs
+++ PicrossCJL/PicrossSolverCLI/Program.cs
@@ -3,29 +3,100 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using PicrossCJL;
+using System.Diagnostics;
 
 namespace PicrossSolverCLI
 {
     class Program
     {
+        static char CellValue2Char(PicrossPuzzle.CellValue cellvalue)
+        {
+            switch (cellvalue)
+            {
+                case PicrossPuzzle.CellValue.Empty:
+                    return ' ';
+                case PicrossPuzzle.CellValue.Filled:
+                    return '#';
+                case PicrossPuzzle.CellValue.Crossed:
+                    return 'x';
+                default:
+                    return ' ';
+            }
+        }
+
         static void Main(string[] args)
         {
-            int[][] linesValue = new int[2][];
-            linesValue[0] = new int[] {1};
-            linesValue[1] = new int[] {1};
+            int[][] linesValue = new int[5][];
+            linesValue[0] = new int[] { 3 };
+            linesValue[1] = new int[] { 4 };
+            linesValue[2] = new int[] { 1 };
+            linesValue[3] = new int[] { 1, 3 };
+            linesValue[4] = new int[] { 2 };
 
-            int[][] columnsValue = new int[2][];
-            columnsValue[0] = new int[] {1};
-            columnsValue[1] = new int[] {1};
+            int[][] columnsValue = new int[5][];
+            columnsValue[0] = new int[] { 1, 1 };
+            columnsValue[1] = new int[] { 2 };
+            columnsValue[2] = new int[] { 4 };
+            columnsValue[3] = new int[] { 2, 2 };
+            columnsValue[4] = new int[] { 2 };
 
-            PicrossPuzzle.CellValue[,] cellsValue = new PicrossPuzzle.CellValue[,] {};
+            PicrossPuzzle.CellValue[,] cellsValue = new PicrossPuzzle.CellValue[5, 5];
 
             PicrossPuzzle puzzle = new PicrossPuzzle(cellsValue, linesValue, columnsValue);
 
             PicrossSolver solver = new PicrossSolver();
 
+            Stopwatch sw = new Stopwatch();
+
+            sw.Restart();
             solver.Solve(puzzle);
-            Console.WriteLine(puzzle);
+            sw.Stop();
+            Console.WriteLine("Time to solve 5x5: {0} ms", sw.ElapsedMilliseconds);
+
+            for (int y = 0; y < puzzle.Size.Height; y++)
+            {
+                for (int x = 0; x < puzzle.Size.Width; x++)
+                {
+                    Console.Write(CellValue2Char(puzzle.Cells[y, x]));
+                }
+                Console.WriteLine();
+            }
+
+            linesValue = new int[7][];
+            linesValue[0] = new int[] { 3 };
+            linesValue[1] = new int[] { 1, 2, 1 };
+            linesValue[2] = new int[] { 2, 3 };
+            linesValue[3] = new int[] { 2, 2 };
+            linesValue[4] = new int[] { 2, 1 };
+            linesValue[5] = new int[] { 1, 2, 1 };
+            linesValue[6] = new int[] { 3, 1 };
+
+            columnsValue = new int[7][];
+            columnsValue[0] = new int[] { 2, 3 };
+            columnsValue[1] = new int[] { 1, 3, 1 };
+            columnsValue[2] = new int[] { 1, 1, 2 };
+            columnsValue[3] = new int[] { 2, 2 };
+            columnsValue[4] = new int[] { 2, 1 };
+            columnsValue[5] = new int[] { 2, 1 };
+            columnsValue[6] = new int[] { 3 };
+
+            cellsValue = new PicrossPuzzle.CellValue[7, 7];
+
+            puzzle = new PicrossPuzzle(cellsValue, linesValue, columnsValue);
+
+            sw.Restart();
+            solver.Solve(puzzle);
+            sw.Stop();
+            Console.WriteLine("Time to solve 7x7: {0} ms", sw.ElapsedMilliseconds);
+
+            for (int y = 0; y < puzzle.Size.Height; y++)
+            {
+                for (int x = 0; x < puzzle.Size.Width; x++)
+                {
+                    Console.Write(CellValue2Char(puzzle.Cells[y, x]));
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
