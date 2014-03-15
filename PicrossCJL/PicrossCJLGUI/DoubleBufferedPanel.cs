cfe53df0cacb94507aeb7583fc258b815d34992a
Loïc Damien <loic.damien+github@gmail.com>
2014-03-15 03:35:32 +0100

    Add a DoubleBufferedPanel class

---
+++ PicrossCJL/PicrossCJLGUI/DoubleBufferedPanel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.ComponentModel;
+
+namespace PicrossCJLGUI
+{
+    public class DoubleBufferedPanel : Panel
+    {
+        public DoubleBufferedPanel()
+            : base()
+        {
+            this.DoubleBuffered = true;
+        }
+    }
+}
