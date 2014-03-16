529e42cb84ba16bf124753d6076b86c5bc112020
Loïc Damien <loic.damien+github@gmail.com>
2014-03-16 21:34:48 +0100

    A whole new and shiny version of the solver.

--- PicrossCJL/PicrossCJL/PicrossSolver.cs
+++ PicrossCJL/PicrossCJL/PicrossSolver.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Drawing;
 using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using System.Diagnostics;
 
 namespace PicrossCJL
 {
@@ -20,7 +22,7 @@ namespace PicrossCJL
         /// <param name="puzzle"></param>
         /// <param name="lineNo"></param>
         /// <returns>True if it make any progress</returns>
-        public bool SolveLine(PicrossPuzzle puzzle, int lineNo)
+        public bool QuickSolveLine(PicrossPuzzle puzzle, int lineNo)
         {
 
             if (puzzle.LinesValues[lineNo].Length > 0)
@@ -69,7 +71,7 @@ namespace PicrossCJL
             return false;
         }
 
-        public bool SolveColumn(PicrossPuzzle puzzle, int columnNo)
+        public bool QuickSolveColumn(PicrossPuzzle puzzle, int columnNo)
         {
             if (puzzle.ColumnsValues[columnNo].Length > 0)
             {
@@ -114,7 +116,7 @@ namespace PicrossCJL
             return false;
         }
 
-       
+
 
         public bool Any(IEnumerable<bool> bools)
         {
@@ -125,12 +127,95 @@ namespace PicrossCJL
             return false;
         }
 
+        private IEnumerable<IEnumerable<int>> GenPossiblesGapLenghts(int nbGap, int nbCrossed, bool first = true)
+        {
+            if (nbGap == 0)
+            {
+                yield return new int[0];
+            }
+            else
+            {
+                int maxGapLength = nbCrossed - nbGap + 1;
+                for (int i = first ? 0 : 1; i <= maxGapLength; i++)
+                {
+                    int[] e = new int[] { i };
+                    foreach (var item in GenPossiblesGapLenghts(nbGap - 1, nbCrossed - i, false))
+                    {
+                        //yield return [i] + item
+                        yield return e.Concat(item);
+                    }
+                }
+            }
+        }
 
-        private bool Backtrack(PicrossPuzzle puzzle, int currentX = 0, int currentY = 0)
+
+        private List<PicrossPuzzle.CellValue[]> GenPossiblesLines(int width, int[] lineValues, int nbCrossed)
+        {
+            if (lineValues.Length == 0)
+            {
+                PicrossPuzzle.CellValue[] line = new PicrossPuzzle.CellValue[width];
+                for (int i = 0; i < width; i++)
+                {
+                    line[i] = PicrossPuzzle.CellValue.Crossed;
+                }
+                List<PicrossPuzzle.CellValue[]> result = new List<PicrossPuzzle.CellValue[]>(capacity: 1);
+                result.Add(line);
+                return result;
+            }
+            else
+            {
+                List<PicrossPuzzle.CellValue[]> result = new List<PicrossPuzzle.CellValue[]>();
+                foreach (var item in GenPossiblesGapLenghts(lineValues.Length, nbCrossed))
+                {
+                    PicrossPuzzle.CellValue[] line = new PicrossPuzzle.CellValue[width];
+                    int idx = 0;
+                    int lineValuesIdx = 0;
+                    foreach (var gapLength in item)
+                    {
+                        int blockLength = lineValues[lineValuesIdx];
+                        for (int i = 0; i < gapLength; i++)
+                        {
+                            line[idx] = PicrossPuzzle.CellValue.Crossed;
+                            idx++;
+                        }
+                        for (int i = 0; i < blockLength; i++)
+                        {
+                            line[idx] = PicrossPuzzle.CellValue.Filled;
+                            idx++;
+                        }
+                        lineValuesIdx++;
+
+                    }
+                    for (; idx < width; idx++)
+                    {
+                        line[idx] = PicrossPuzzle.CellValue.Crossed;
+                    }
+                    result.Add(line);
+                }
+                return result;
+                // GenPossiblesGapLenghts(lineValues.Length, nbCrossed)
+            }
+        }
+
+
+        private void SolveSteps(PicrossPuzzle puzzle, int w, int h, List<PicrossPuzzle.CellValue[]>[] possibleLinesValues, List<PicrossPuzzle.CellValue[]>[] possiblesColumnsValues)
+        {
+            //TODO: work only on affected row/column, by using conccurentBag
+            bool[] progressLines = new bool[h];
+            bool[] progressColumn = new bool[w];
+            do
+            {
+                Parallel.For(0, h, i => { progressLines[i] = SolveLine(puzzle, w, i, possibleLinesValues[i]); });
+                Parallel.For(0, w, i => { progressColumn[i] = SolveColumn(puzzle, h, i, possiblesColumnsValues[i]); });
+            } while (Any(progressLines) || Any(progressColumn));
+        }
+
+
+        private bool Backtrack(PicrossPuzzle puzzle, int w, int h, List<PicrossPuzzle.CellValue[]>[] possibleLinesValues, List<PicrossPuzzle.CellValue[]>[] possiblesColumnsValues, int currentX = 0, int currentY = 0)
         {
 
             //TODO: don't recheck past line
-            //TODO: TODO:eliminate call to GetPuzzleState b y only checking affected row
+            //TODO: TODO:eliminate call to GetPuzzleState by only checking affected row and column
             //algo:
             // backtracking, remplir ce dont est sur en fonction de l'état de la grille entre chaque appel recursif
 
@@ -143,14 +228,13 @@ namespace PicrossCJL
             else if (puzzleState == PicrossPuzzle.PuzzleState.Incorrect)
                 return false;
 
-            Size s = puzzle.Size;
-            int w = s.Width;
-            int h = s.Height;
+
+            SolveSteps(puzzle, w, h, possibleLinesValues, possiblesColumnsValues);
 
             // pour chaque cellule avec des doutes:
             // essayer remplir la cellule, essayer de résoudre (appel recursif à solve)
             // si resolutuion ok -> fini
-            // sinon -> cellule crossed, essayer de remplir la prochiane, etc.
+            // sinon -> cellule crossed, essayer de remplir la prochaine, etc.
             int x = currentX;
             for (int y = currentY; y < h; y++)
             {
@@ -170,12 +254,12 @@ namespace PicrossCJL
                                 puzzle.Cells[y, x] = PicrossPuzzle.CellValue.Crossed;
                             else
                             {
-                                // lineState and columnState is either incomplte or finished
+                                // lineState and columnState is either incomplete or finished
 
-                                //if(y == height-1 && x = == width -1 && lineState == PicrossPuzzle.PuzzleState.Finished && columnState == PicrossPuzzle.PuzzleState.Finished)
+                                //if(y == height-1 && x = == height -1 && lineState == PicrossPuzzle.PuzzleState.Finished && columnState == PicrossPuzzle.PuzzleState.Finished)
 
                                 PicrossPuzzle tmpPuzzle = new PicrossPuzzle(puzzle);
-                                if (Backtrack(tmpPuzzle, x, y))
+                                if (Backtrack(tmpPuzzle, w, h, PossibilitiesDeepCopy(possibleLinesValues), PossibilitiesDeepCopy(possiblesColumnsValues), x, y))
                                 {
                                     puzzle.Cells = tmpPuzzle.Cells;
                                     return true;
@@ -188,10 +272,127 @@ namespace PicrossCJL
                 }
                 x = 0;
             }
-
-            return false;
+            
+            return puzzle.GetPuzzleState() == PicrossPuzzle.PuzzleState.Finished;
         }
 
+        private bool SolveLine(PicrossPuzzle puzzle, int width, int lineNo, List<PicrossPuzzle.CellValue[]> possibilities/*, ConcurrentBag<int> affectedColumns*/)
+        {
+            bool progress = false;
+            for (int i = 0; i < width; i++)
+            {
+                PicrossPuzzle.CellValue value = puzzle.Cells[lineNo, i];
+                if (value != PicrossPuzzle.CellValue.Empty)
+                {
+                    //if possiblity is different in a cell we are sure of, this possiblity is wrong, we remove it
+                    for (int j = 0; j < possibilities.Count; j++)
+                    {
+                        if (possibilities[j][i] != value)
+                        {
+                            possibilities.RemoveAt(j);
+                            j--;
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < width; i++)
+            {
+                PicrossPuzzle.CellValue value = puzzle.Cells[lineNo, i];
+                if (value == PicrossPuzzle.CellValue.Empty)
+                {
+                    //if a cell value is the same in every possibiliy it is the corrct one
+                    foreach (var possiblity in possibilities)
+                    {
+                        PicrossPuzzle.CellValue possiblityValue = possiblity[i];
+                        if (value == PicrossPuzzle.CellValue.Empty)
+                            value = possiblityValue;
+                        else
+                        {
+                            if (possiblityValue != value)
+                            {
+                                value = PicrossPuzzle.CellValue.Empty;
+                                break;
+                            }
+                        }
+                    }
+                    if (value != PicrossPuzzle.CellValue.Empty)
+                    {
+                        puzzle.Cells[lineNo, i] = value;
+                        //affectedColumns.Add(i);
+                        progress = true;
+                    }
+                }
+            }
+            return progress;
+        }
+
+        private bool SolveColumn(PicrossPuzzle puzzle, int height, int columnNo, List<PicrossPuzzle.CellValue[]> possibilities/*, ConcurrentBag<int> affectedLines*/)
+        {
+            bool progress = false;
+            for (int i = 0; i < height; i++)
+            {
+                PicrossPuzzle.CellValue value = puzzle.Cells[i, columnNo];
+                if (value != PicrossPuzzle.CellValue.Empty)
+                {
+                    //if possiblity is different in a cell we are sure of, this possiblity is wrong, we remove it
+                    for (int j = 0; j < possibilities.Count; j++)
+                    {
+                        if (possibilities[j][i] != value)
+                        {
+                            possibilities.RemoveAt(j);
+                            j--;
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < height; i++)
+            {
+                PicrossPuzzle.CellValue value = puzzle.Cells[i, columnNo];
+                if (value == PicrossPuzzle.CellValue.Empty)
+                {
+                    //if a cell value is the same in every possibiliy it is the corrct one
+                    foreach (var possiblity in possibilities)
+                    {
+                        PicrossPuzzle.CellValue possiblityValue = possiblity[i];
+                        if (value == PicrossPuzzle.CellValue.Empty)
+                            value = possiblityValue;
+                        else
+                        {
+                            if (possiblityValue != value)
+                            {
+                                value = PicrossPuzzle.CellValue.Empty;
+                                break;
+                            }
+                        }
+                    }
+                    if (value != PicrossPuzzle.CellValue.Empty)
+                    {
+                        puzzle.Cells[i, columnNo] = value;
+                        //affectedLines.Add(i);
+                        progress = true;
+                    }
+                }
+            }
+            return progress;
+        }
+
+        private List<PicrossPuzzle.CellValue[]>[] PossibilitiesDeepCopy(List<PicrossPuzzle.CellValue[]>[] possiblities)
+        {
+            List<PicrossPuzzle.CellValue[]>[] copy = new List<PicrossPuzzle.CellValue[]>[possiblities.Length];
+            for (int i = 0; i < possiblities.Length; i++)
+            {
+                copy[i] = new List<PicrossPuzzle.CellValue[]>(possiblities[i].Select(line => (PicrossPuzzle.CellValue[])line.Clone()));
+            }
+            return copy;
+        }
+
+        /// <summary>
+        /// Solve a puzzle.
+        /// </summary>
+        /// <param name="puzzle">The puzzle to solve</param>
+        /// <returns></returns>
         public bool Solve(PicrossPuzzle puzzle)
         {
             //algo:
@@ -205,27 +406,24 @@ namespace PicrossCJL
             else if (puzzleState == PicrossPuzzle.PuzzleState.Incorrect)
                 return false;
 
-            Size s = puzzle.Size;
-            int w = s.Width;
-            int h = s.Height;
+            int w = puzzle.Width;
+            int h = puzzle.Height;
 
-            // remplir ce dont on n'est sur
+            Parallel.For(0, h, i => QuickSolveLine(puzzle, i));
+            Parallel.For(0, w, i => QuickSolveColumn(puzzle, i));
 
-            //bool[] progressLines = new bool[h];
-            //bool[] progressColumn = new bool[w];
-            //do
-            //{
-            //  Parallel.For(0, h, i => { progressLines[i] = SolveLine(puzzle, i); });
-            //  Parallel.For(0, w, i => { progressColumn[i] = SolveColumn(puzzle, i); });
-            //} while (Any(progressLines) || Any(progressColumn));
+            Stopwatch sw  = new Stopwatch();
+            sw.Start();
+            List<PicrossPuzzle.CellValue[]>[] possibleLinesValues = new List<PicrossPuzzle.CellValue[]>[h];
+            List<PicrossPuzzle.CellValue[]>[] possiblesColumnsValues = new List<PicrossPuzzle.CellValue[]>[w];
+            Parallel.For(0, h, i => possibleLinesValues[i] = GenPossiblesLines(w, puzzle.LinesValues[i], w - puzzle.NbFilledByLines[i]));
+            Parallel.For(0, h, i => possiblesColumnsValues[i] = GenPossiblesLines(h, puzzle.ColumnsValues[i], h - puzzle.NbFilledByColumns[i]));
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedMilliseconds);
 
-            Parallel.For(0, h, i => SolveLine(puzzle, i));
-            Parallel.For(0, w, i => SolveColumn(puzzle, i));
+            //SolveSteps(puzzle, w, h, possibleLinesValues, possiblesColumnsValues);
 
-            //Console.WriteLine("SolvelLine");
-            //Console.WriteLine(puzzle);
-
-            if (Backtrack(puzzle))
+            if (Backtrack(puzzle, w, h, PossibilitiesDeepCopy(possibleLinesValues), PossibilitiesDeepCopy(possiblesColumnsValues)))
             {
 
                 Parallel.For(0, h, y =>
